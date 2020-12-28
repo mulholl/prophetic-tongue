c3d829334e66a1b71f964883354525f87bd3e67f
Ian Mulholland <hi@ianmulholland.com>
2020-12-28 19:01:04 +0100

    Number of names to generate can now be specified through input argument

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
 // A simple tool for generating meaningless project names
+use std::env;
 use rand::{self, Rng};
 
 fn main() {
+    // Get the command line arguments
+    let args: Vec<String> = env::args().collect();
+
+    let mut num: i32 = 1; // number of names to generate (default = 1)
+
+    // Check whether any command line arguments were given
+    if args.len() > 1{
+        let num_str = &args[1]; // This should be the desired number of names
+
+        // Try to convert the argument to the type of num
+        num = match num_str.parse() {
+            Ok(n) => {
+                n
+            },
+            Err(_) => {
+                eprintln!("error: second argument not an integer");
+                help();
+                return;
+            },
+        };
+    
+    println!("Generating {} names", num);
+
+    }
+
     // Import the wordlists as strings
     let adjectives = include_str!("adj");
     let nouns = include_str!("noun");
@@ -12,17 +38,25 @@ fn main() {
 
     // Find the length of each vector and use it to select a random noun and adjective
     let num_adjectives = adjectives.len();
-    let adjective_index = rand::thread_rng().gen_range(0, num_adjectives);
-    let adjective = &adjectives[adjective_index];
-
     let num_nouns = nouns.len();
-    let noun_index = rand::thread_rng().gen_range(0, num_nouns);
-    let noun = &nouns[noun_index];
-    
-    // println!("{} adjectives and {} nouns", num_adjectives, num_nouns);
-    // println!("Picked noun number {} and noun number {}", adjective_index, noun_index);
 
-    // Concatenate the selected adjective and noun into a suggested name and print it
-    let suggested_name = [adjective, " ", noun].concat();
-    println!("{}", suggested_name);
+    for _ in 0..num{
+        let adjective_index = rand::thread_rng().gen_range(0, num_adjectives);
+        let adjective = &adjectives[adjective_index];
+
+        let noun_index = rand::thread_rng().gen_range(0, num_nouns);
+        let noun = &nouns[noun_index];
+    
+        // println!("{} adjectives and {} nouns", num_adjectives, num_nouns);
+        // println!("Picked noun number {} and noun number {}", adjective_index, noun_index);
+
+        // Concatenate the selected adjective and noun into a suggested name and print it
+        let suggested_name = [adjective, " ", noun].concat();
+        println!("{}", suggested_name);
+    }
+}
+
+fn help() {
+    println!("Usage: prophetic_tongue [NUMBER; default = 1]
+    Generate NUMBER random names.");
 }
